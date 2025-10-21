c7b8659bb73d5590e453babaf773162ef162c691
julianoz <julianoz@weg.net>
2025-10-21 15:21:04 -0300

    Primeiro commit

---
+++ imoveis.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("imoveis.json")
+    .then((res) => {
+      if (!res.ok) {
+        // Verifica se a resposta da rede foi bem-sucedida (status 200 OK)
+        throw new Error(`HTTP error! status: ${res.status}`);
+      }
+      return res.json();
+    })
+    .then((imoveis) => {
+      const container = document.getElementById("lista-imoveis");
+      if (!container) {
+        console.error("Elemento #lista-imoveis não encontrado.");
+        return;
+      }
+
+      imoveis.forEach((imovel) => {
+        const card = document.createElement("div");
+        card.className = "imovel-card";
+        card.innerHTML = `
+          <img src="${imovel.imagens[0]}" class="imovel-card-image">
+          <div class="imovel-card-content">
+            <h3>${imovel.tipo}</h3>
+            <div class="imovel-details">
+              <p><span class="cod">De:</span></p>
+              <div class="imovel-location">
+                ${imovel.localizacao}
+              </div>
+            </div>
+            <div class="imovel-price">${imovel.valor}</div>
+          </div>
+          <a href="https://wa.me/5547991175167?text=Olá! Tenho interesse em informações sobre carros PCD." target="_blank" class="imovel-button">WhatsApp</a>
+        `;
+        container.appendChild(card);
+      });
+    })
+    .catch((error) => console.error("Erro ao carregar imóveis:", error));
+});
